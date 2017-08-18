3127a0ff1f433d352a5421ab5ad0526a2b4225b3
Antonio <antonio.di.giorgio94@gmail.com>
2017-08-18 18:50:29 +0200

    Update style of RigaFattura dashboard

--- src/Kirtek/TestBundle/Admin/RigaFatturaAdmin.php
+++ src/Kirtek/TestBundle/Admin/RigaFatturaAdmin.php
@@ -10,54 +10,50 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 
 class RigaFatturaAdmin extends AbstractAdmin {
 
-  protected function configureFormFields(FormMapper $formMapper)
-  {
-    $formMapper
-
-    ->tab('DatiFattura')
-        ->with('Dati Fattura', array(
-          'class' => 'col-md-8'
-        ))
-          ->add('id_fattura', 'sonata_type_model', array(
-              'class' => 'Kirtek\TestBundle\Entity\Fattura',
-              'property' => 'id',
-          ), array(
-            'label' => 'ID Fattura'
-          ))
-          ->add('descrizione', 'text', array(
-              'label' => 'Descrizione'
-          ))
-          ->add('quantita', 'integer', array(
-              'label' => 'Quantita\''
-          ))
-        ->end()
-        ->with('Importo', array(
-          'class' => 'col-md-4'
-        ))
-          ->add('importo', 'number', array(
-              'label' => 'Importo'
-          ))
-          ->add('importo_iva', 'number', array(
-              'label' => 'Importo IVA'
-          ))
-          ->add('totale', 'number', array(
-              'label' => 'Totale'
-          ))
-        ->end()
-      ->end()
-    ;
-
-  }
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Dati Fattura', array(
+              'class' => 'col-md-8'
+            ))
+                  ->add('id_fattura', 'sonata_type_model', array(
+                      'class' => 'Kirtek\TestBundle\Entity\Fattura',
+                      'property' => 'id',
+                  ), array(
+                    'label' => 'ID Fattura'
+                  ))
+                  ->add('descrizione', 'text', array(
+                      'label' => 'Descrizione'
+                  ))
+                  ->add('quantita', 'integer', array(
+                      'label' => 'Quantita\''
+                  ))
+            ->end()
+            ->with('Importo', array(
+              'class' => 'col-md-4'
+            ))
+                  ->add('importo', 'number', array(
+                      'label' => 'Importo'
+                  ))
+                  ->add('importo_iva', 'number', array(
+                      'label' => 'Importo IVA'
+                  ))
+                  ->add('totale', 'number', array(
+                      'label' => 'Totale'
+                  ))
+            ->end()
+        ;
+    }
 
   // Fields to be shown on lists
-  protected function configureListFields(ListMapper $listMapper)
-  {
-      $listMapper
-          ->add('id')
-          ->add('id_fattura')
-          ->add('totale')
-          ->add('descrizione')
-     ;
-  }
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('id_fattura')
+            ->add('totale')
+            ->add('descrizione')
+        ;
+    }
 
 }
