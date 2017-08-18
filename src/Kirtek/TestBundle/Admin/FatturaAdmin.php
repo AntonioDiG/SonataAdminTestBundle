30044d766cce43b266990bf073fca808129011dc
Antonio <antonio.di.giorgio94@gmail.com>
2017-08-18 18:49:50 +0200

    Improved indentation of FatturaAdmin

--- src/Kirtek/TestBundle/Admin/FatturaAdmin.php
+++ src/Kirtek/TestBundle/Admin/FatturaAdmin.php
@@ -10,25 +10,25 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 
 class FatturaAdmin extends AbstractAdmin {
 
-  protected function configureFormFields(FormMapper $formMapper)
-  {
-    $formMapper
-      ->add('data', 'datetime', array(
-          'label' => 'Data'
-      ))
-      ->add('numero', 'integer', array(
-          'label' => 'Numero'
-      ))
-      ->add('id_cliente', 'integer', array(
-          'label' => 'ID Cliente'
-      ))
-   ;
-  }
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('data', 'datetime', array(
+            'label' => 'Data'
+            ))
+            ->add('numero', 'integer', array(
+            'label' => 'Numero'
+            ))
+            ->add('id_cliente', 'integer', array(
+            'label' => 'ID Cliente'
+            ))
+        ;
+    }
 
 // Fields to be shown on filter forms
 protected function configureDatagridFilters(DatagridMapper $datagridMapper)
 {
-   $datagridMapper
+    $datagridMapper
         ->add('data', null, array(), 'sonata_type_datetime_picker', array(
             'format' => 'dd/MM/yyyy',
             'dp_side_by_side' => true,
@@ -36,28 +36,28 @@ protected function configureDatagridFilters(DatagridMapper $datagridMapper)
             'dp_use_seconds' => false,
         ))
         ->add('id_cliente')
-   ;
+    ;
 }
 
-// Fields to be shown on lists
-protected function configureListFields(ListMapper $listMapper)
-{
-    $listMapper
-        ->add('id')
-        ->add('data')
-        ->add('id_cliente')
-   ;
-}
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('data')
+            ->add('id_cliente')
+       ;
+    }
 
-// Fields to be shown on show action
-protected function configureShowFields(ShowMapper $showMapper)
-{
-    $showMapper
-    ->add('id')
-    ->add('data')
-    ->add('id_cliente')
-    ->add('numero')
-   ;
-}
+    // Fields to be shown on show action
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('data')
+            ->add('id_cliente')
+            ->add('numero')
+       ;
+    }
 
 }
